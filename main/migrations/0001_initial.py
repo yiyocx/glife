da0bef506627b14237ba7662d07123df981b1770
Yiyo Castillo <yiyocx@gmail.com>
2015-09-10 10:35:10 -0500

    Creados modelos Tag y Document

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('file', models.FileField(upload_to=b'')),
+                ('title', models.CharField(max_length=140)),
+                ('description', models.TextField(blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('owner', models.ForeignKey(related_name='documents', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.SlugField(unique=True, max_length=30)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='document',
+            name='tags',
+            field=models.ManyToManyField(to='main.Tag'),
+        ),
+    ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
+# coding=utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    slug = models.SlugField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+def calculate_path(self, filename):
+    """
+    Método que calcula la ruta donde se almacenaran
+    los documentos subidos por los usuarios
+    """
+    return 'users/%s/documents/%s' % (self.owner.id, filename)
+
+
+class Document(models.Model):
+    file = models.FileField(upload_to=calculate_path)
+    title = models.CharField(max_length=140)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, related_name='documents')
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
