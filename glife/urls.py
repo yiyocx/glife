0234ccc786fe6b86923e498b77ac0a016d497728
Yiyo Castillo <yiyocx@gmail.com>
2015-09-10 13:28:45 -0500

    Creado endpoint para listar usuarios del sistema
    /users

--- glife/urls.py
+++ glife/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from main import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^users/$', views.ListUsers.as_view()),
 ]
+
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# coding=utf-8
+from rest_framework import authentication, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from main.serializers import UserSerializer
+
+from models import User
+
+
+class ListUsers(generics.ListCreateAPIView):
+    """
+    Lista todos los usuarios del sistema
+
+    * Requiere autenticación por Token
+    * Solo los usuarios administrativos pueden acceder a esta vista
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication, )
+    permission_classes = (permissions.IsAdminUser, )
