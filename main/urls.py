5a09798fb3e57167013ce3012de3dd77d1539b83
Yiyo Castillo <yiyocx@gmail.com>
2015-09-17 15:38:30 -0500

    Se agrega autenticación por Token

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from rest_framework.authtoken import views as token_views
 
 from main import views
 
 urlpatterns = [
     url(r'^users/$', views.ListUsers.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetailView.as_view(), name='user-detail'),
+    url(r'^api-token-auth/', token_views.obtain_auth_token),
 ]
