982d9f34ea04f7d16eed4941a1b722346410e1f6
Yiyo Castillo <yiyocx@gmail.com>
2015-09-18 18:02:04 -0500

    Agregada validación de contraseña al usuario

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 
 from api.models import Document, Tag
-from custom_auth.models import User
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,14 +21,32 @@ class DocumentSerializer(serializers.HyperlinkedModelSerializer):
         model = Document
 
 
+def validate_password(self, password):
+    print('Hola %s %s' % (self, password))
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     documents = serializers.HyperlinkedRelatedField(many=True, view_name='document-detail', read_only=True)
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     confirm_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('url', 'email', 'first_name', 'last_name', 'phone_number', 'date_of_birth',
                   'password', 'confirm_password', 'is_active', 'documents')
         read_only_fields = ('is_active', )
         #exclude = ('groups', 'user_permissions', )
+
+    def validate(self, attrs):
+        password = attrs['password']
+        confirm_password = attrs['confirm_password']
+        if password == confirm_password:
+            del attrs['confirm_password']
+            return attrs
+        raise ValidationError('Password and confirm password must be the same')
+
+
+
+
+
+
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from rest_framework.authtoken import views as token_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 from api import views
 
 urlpatterns = [
     url(r'^users/$', views.ListUsers.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetailView.as_view(), name='user-detail'),
-    url(r'^api-token-auth/', token_views.obtain_auth_token),
+    url(r'^api-token-auth/', obtain_auth_token),
 ]
