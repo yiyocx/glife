43ed2a40df5f7ddd1c3a7b56f19210e30e94a068
Yiyo Castillo <yiyocx@gmail.com>
2015-09-23 10:23:03 -0500

    Agregada migración necesaria

---
+++ custom_auth/migrations/0005_auto_20150923_0724.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('custom_auth', '0004_auto_20150918_1527'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='phone_number',
+            field=models.CharField(blank=True, max_length=15, null=True, validators=[django.core.validators.RegexValidator(regex=b'^\\+?1?\\d{9,15}$', message=b"Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")]),
+        ),
+    ]
